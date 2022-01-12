c02b5185605da3426a4d9402f6a20526ba328cd9
Mauricio <mauriciomeza95@hotmail.com>
2022-01-12 11:52:43 -0600

    Se agregaron propiedades

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ console.log(mexico.comida)
 
 //BRACKET NOTATION - Acceder a las propiedades
 console.log(mexico["comida"])
+
+const olympicRecords = {
+    athletic100Men: "Justin Gatlin",
+    athleticLongJumpMen: "Mike Powell",
+    swimming200Men: "Michael Phelps",
+    "swimming 200 Women": "Ariadna Titmus"
+}
+
+console.log(olympicRecords.athletic100Men)
+console.log(olympicRecords["athletic100Men"])
+console.log(olympicRecords["swimming 200 Women"])
+
+
+// B. AGREGAR PROPIEDADES
+
+const refresco = {
+	nombre: "Coca cola",
+	color: "Oscuro",
+	precio: 12,
+	tamanos: [600, 1000]
+}
+
+refresco.envase = "Plástico"
+
+console.log("Objeto de refresco:", refresco)
+
+refresco["competidor"] = "Pepsi"
+
+console.log(refresco)
