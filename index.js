f4de76854f66b42bce0ebabfb98a4eb09f02e054
Mauricio <mauriciomeza95@hotmail.com>
2022-01-12 11:34:03 -0600

    Acceso a propiedades

---
+++ index.js
@@ -0,0 +1,13 @@
+// OBJETOS 
+//NO PRIMITIVOS - COMPUESTOS POR PROPIEDADES Y VALORES
+const mexico = {
+    comida: "tacos" //Propiedad: comida  y Valor: tacos
+}
+
+console.log(mexico)
+
+//DOT NOTATION -Acceder a las propiedades
+console.log(mexico.comida)
+
+//BRACKET NOTATION - Acceder a las propiedades
+console.log(mexico["comida"])
---
+++ readme.md
@@ -0,0 +1,4 @@
+# DOCUMENTACIÓN
+## Pre-requisitos
+- Instalar como extensión en VSCode:
+    -[Code Runner](https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner)
